7401fed623c1632bb3e64a60d62fd22a0b1461a3
Nithya <159105947+NithyaLavSetlur@users.noreply.github.com>
2025-05-28 16:18:05 +1000

    Checking out previous commit
    Commit 947c90a0658acbd87dd3a68f0dea9ffe25f6101b

--- app/src/main/java/auth/SignUpFragment.kt
+++
@@ -1,4 +0,0 @@
-package auth
-
-class SignUpFragment {
-}
--- app/src/main/java/models/Subject.kt
+++
@@ -1,9 +0,0 @@
-package com.yourdomain.stulogandroidapp.models
-
-data class Subject(
-    val id: String = "",
-    val name: String = "",
-    val color: Int = 0,
-    val progress: Float = 0f, // 0 to 1
-    val tasks: List<Task> = emptyList()
-)
